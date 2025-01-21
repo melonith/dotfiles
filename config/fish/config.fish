1139ef60641ebb1f667b14ace37d6ebc4cdf4611
melonith <lonnniefr@proton.me>
2025-01-20 21:00:53 -0500

    Misspelled XDG_DATA_HOME

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ set -g fish_greeting
 
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_DATE_HOME "$HOME/.local/share"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
 
 set -gpx PATH "$HOME/bin"
